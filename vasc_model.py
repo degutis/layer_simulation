a52502be8750db2e48f4f74d94514a9db845ca31
Karolis <j.karolis.degutis@gmail.com>
2024-09-26 12:55:19 +0200

    small change to vasc model

--- vasc_model.py
+++ vasc_model.py
@@ -38,7 +38,7 @@ class vascModel:
         lower_triangle = np.tril(np.ones((self.layers, self.layers)), k=-1)
         self.matrix = self.matrix + (1/self.p2)*lower_triangle
 
-        self.outputMatrix = self.matrix * self.original_response
+        self.outputMatrix = self.matrix * self.orig_response
 
     def __calculate_p2t__(p2t_model, n, n_model):
         
