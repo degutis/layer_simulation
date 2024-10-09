6bd97ad76d1e9a5f242acc61456951519d37427b
Karolis <j.karolis.degutis@gmail.com>
2024-10-09 19:06:52 +0200

    created a script that runs the analysis and plots the results

---
+++ plotResults.py
@@ -0,0 +1,39 @@
+import seaborn as sns
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def plotViolin(accuracy, rho_values, nSize, title):
+
+    numLayers = accuracy.shape[0]
+    numParams = accuracy.shape[2]
+    numIterations = accuracy.shape[1]
+
+    accuracy_flat = accuracy.transpose(0,2,1).reshape(numLayers * numParams * numIterations)
+    layers = np.repeat(np.arange(1, numLayers+1), numParams * numIterations) 
+    parameters = np.tile(np.repeat(rho_values, numIterations), numLayers)  
+    accuracy_values = np.tile(np.arange(1, numIterations+1), numLayers * numParams)
+
+    df = pd.DataFrame({
+        'Layer': layers,
+        'Rho': parameters,
+        'Accuracy': accuracy_flat
+    })
+
+    palette = sns.color_palette("Set2", 3)  
+    fig, axes = plt.subplots(nrows=nSize[0], ncols=nSize[1], figsize=(20, 6), sharey=True)
+    for i, rho in enumerate(rho_values):
+        ax = axes[i]        
+        sns.violinplot(x='Layer', y='Accuracy', data=df[df['Rho'] == rho], 
+                    ax=ax, hue='Layer', palette=palette, inner = "points", split=False, legend=False)
+        ax.set_title(f'Rho = {rho}')
+        ax.set_xlabel('Layer')
+        if i == 0:
+            ax.set_ylabel('Accuracy')
+        else:
+            ax.set_ylabel('')
+
+    plt.tight_layout()
+    plt.savefig(f"../derivatives/results/{title}.png")
+
+
---
+++ runSimulation.py
@@ -0,0 +1,43 @@
+import simulation as sim
+import vasc_model as vm
+import plotResults
+
+import numpy as np
+
+
+iterations=30
+layers = 3
+rho_values = [0.7, 0.8, 0.9, 1, 1.1, 1.2, 1.3]
+rval = len(rho_values)
+
+numTrials_per_class = 50
+beta = 0.02
+
+accuracy_samePattern = np.empty((layers,iterations, rval))
+accuracy_onePatternDeep = np.empty((layers,iterations,rval))
+accuracy_onePatternMiddle = np.empty((layers,iterations,rval))
+accuracy_onePatternSup = np.empty((layers,iterations,rval))
+
+for it in range(iterations):
+    print(it)
+    for i,r in enumerate(rho_values):
+    
+        vox = sim.VoxelResponses(it,r,r, numTrials_per_class=numTrials_per_class, beta=beta)
+    
+        drainedResp = vox.calculateDrainingEffect_samePatternAcrossLayers(layers=layers)    
+        accuracy_samePattern[:,it,i] = vox.runSVM_classifier_acrossLayers(drainedResp.outputMatrix)   
+    
+        drainingVeinDeep = vox.calculateDrainingEffect_patternOnlyInSingleLayer(0, it+10, layers=layers)
+        accuracy_onePatternDeep[:,it,i] = vox.runSVM_classifier_acrossLayers(drainingVeinDeep.outputMatrix)   
+    
+        drainingVeinMiddle = vox.calculateDrainingEffect_patternOnlyInSingleLayer(1, it+20, layers=layers)
+        accuracy_onePatternMiddle[:,it,i] = vox.runSVM_classifier_acrossLayers(drainingVeinMiddle.outputMatrix)   
+    
+        drainingVeinSup = vox.calculateDrainingEffect_patternOnlyInSingleLayer(2, it+30, layers=layers)
+        accuracy_onePatternSup[:,it,i] = vox.runSVM_classifier_acrossLayers(drainingVeinSup.outputMatrix)   
+
+nSize = [1,7]
+plotResults.plotViolin(accuracy_samePattern, rho_values, nSize, "SamePatternAcrossLayers")
+plotResults.plotViolin(accuracy_onePatternDeep, rho_values, nSize, "OnePatternDeep")
+plotResults.plotViolin(accuracy_onePatternMiddle, rho_values, nSize, "OnePatternMiddle")
+plotResults.plotViolin(accuracy_onePatternSup, rho_values, nSize, "OnePatternSup")
