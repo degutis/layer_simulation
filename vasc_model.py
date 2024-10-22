c1a34c2dec7b6c98abd9188d0b954940f0b19633
Karolis <j.karolis.degutis@gmail.com>
2024-10-22 15:52:59 +0200

    added matrix combining signal from adjacent layers

--- vasc_model.py
+++ vasc_model.py
@@ -26,20 +26,23 @@ class vascModel:
         Calculates the value of p2t based on the base value, number of layers, and base number of layers.
     """
 
-    def __init__(self, orig_response, layers):
+    def __init__(self, orig_response, layers, laminarPSF=0.04):
 
         self.layers = layers
         self.orig_response = orig_response
+        self.laminarPSF = laminarPSF
 
         if layers!=10:
             self.p2t = self.__calculate_p2t__(6.3, layers, 10)
         else:
             self.p2t = 6.3
 
-        self.matrix = np.eye(self.layers)
+        matrix = np.eye(self.layers)
         lower_triangle = np.tril(np.ones((self.layers, self.layers)), k=-1)
-        self.matrix = self.matrix + (1/self.p2t)*lower_triangle
-        self.outputMatrix = np.einsum('ij,jkl->ikl', self.matrix, self.orig_response)
+        self.p2t_matrix = matrix + (1/self.p2t)*lower_triangle
+        self.PSF_3D = self.__calculate_3DPSF_matrix__(matrix, self.laminarPSF)
+        self.laminarSpread = np.dot(self.p2t_matrix, self.PSF_3D)
+        self.outputMatrix = np.einsum('ij,jkl->ikl', self.laminarSpread, self.orig_response)
 
 
 
@@ -57,3 +60,17 @@ class vascModel:
         float : The calculated value of p2t
         """
         return np.round((n / n_model) * p2t_model + (n_model - n) / (2 * n_model),2)
+    
+
+    def __calculate_3DPSF_matrix__(matrix, num):
+        
+        x = matrix.shape[0]
+
+        matrix += np.diag([num] * (x - 1), k=1)  
+        matrix += np.diag([num] * (x - 1), k=-1) 
+
+        matrix += np.diag([num / 2] * (x - 2), k=2)  
+        matrix += np.diag([num / 2] * (x - 2), k=-2) 
+    
+        return matrix
+
